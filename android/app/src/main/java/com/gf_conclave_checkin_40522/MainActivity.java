326683639430d2bd1ed6dbbea4e4275c9de9d999
Crowdbotics <team@crowdbotics.com>
2023-05-07 18:43:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gf_conclave_checkin_40522/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gf_conclave_checkin_40522;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gf_conclave_checkin_40522";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gf-conclave-checkin-40522",
+  url: "https://gf-conclave-checkin-40522.botics.co",
+  api: "https://gf-conclave-checkin-40522.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
